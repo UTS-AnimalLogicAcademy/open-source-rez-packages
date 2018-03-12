c647f7d7eb32d27da8fd50b605925929cd416b53
Benjamin Skinner <benjamin.skinner@student.uts.edu.au>
2018-03-12 12:30:19 +1100

    Created USD build scripts for version 0.8.4

---
+++ usd/0.8.4/clean.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+rm v0.8.4.tar.gz
+rm -rf USD-0.8.4
+rm -rf build
+
---
+++ usd/0.8.4/setup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+wget https://github.com/PixarAnimationStudios/USD/archive/v0.8.4.tar.gz
+tar -xvf v0.8.4.tar.gz
+
