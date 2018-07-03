8b578dfec05088d99fd3df40db309100293b2b66
Daniel Flood <daniel.flood-1@uts.edu.au>
2018-07-03 11:21:24 +1000

    added usd-0.8.5

---
+++ usd/0.8.5/package.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+name = 'usd'
+
+version = '0.8.5'
+
+requires = [
+    'alembic-1.5',
+    'boost-1.55',
+    'tbb-4.4.6',
+    'opensubdiv-3.2',
+    'ilmbase-2.2',
+    'openexr-2.2',
+    'pyilmbase-2.2',
+    'oiio-1.8',
+    'ptex-2.0',
+]
+
+build_requires = [
+    'cmake-3.2',
+    'pyside-1.2',
+    'maya-2017'
+]
+
+variants = [['platform-linux', 'arch-x86_64']]
+
+def commands():
+    env.PYTHONPATH.append('{root}/lib/python')
+    env.LD_LIBRARY_PATH.append('{root}/lib/')
+    appendenv('PATH', '{root}/bin/')
+
+
