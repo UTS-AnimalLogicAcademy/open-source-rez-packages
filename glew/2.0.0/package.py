e7c61225a11f37dbbd6002c8eec0a89f22c04217
Daniel Flood <daniel.flood-1@uts.edu.au>
2018-07-24 14:41:10 +1000

    env vars for glew

--- glew/2.0.0/package.py
+++ glew/2.0.0/package.py
@@ -4,8 +4,7 @@ name = 'glew'
 
 version = '2.0.0'
 
-variants = [['platform-linux', 'arch-x86_64']]
-
 def commands():
     env.LD_LIBRARY_PATH.append('{this.root}/lib64')
     appendenv('PKG_CONFIG_PATH', '{root}/lib/pkgconfig/')
+    env.LD_PRELOAD.append('{this.root}/lib64')
