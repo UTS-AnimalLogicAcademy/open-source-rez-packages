463bd267d0ee1ae940483a84b7a6882373279dad
Daniel Flood <daniel.flood-1@uts.edu.au>
2018-12-07 23:50:18 +1100

    bugfix in usd_houdini

--- usd_houdini/18.11/rez-release.sh
+++ usd_houdini/18.11/rez-release.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+source version.sh
+
 # this will build (if not built already) and release to the configured rez release location
 # the extra flags are because we are not developing this code with git, we're just building
 # someone else's code.  
