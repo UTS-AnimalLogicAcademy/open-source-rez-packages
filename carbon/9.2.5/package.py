ac0f15b80ed51c951b180e4be1038380acaa5505
Ben <benjamin.skinner@uts.edu.au>
2019-08-20 13:33:55 +1000

    Updated carbon's package.py

--- carbon/9.2.5/package.py
+++ carbon/9.2.5/package.py
@@ -9,7 +9,7 @@ variants = [['platform-linux', 'arch-x86_64', 'houdini-17.5.327']]
 def commands():
     env.SIMCARBONTOOL_PATH.set('file:///opt/carbon/9.2.5/hfs17.5.327/numerion/SIM_CarbonTool/')
     env.NUMERION_SOFTWARE_LICENSE_PATH.set('6200@roz.ala.int.uts.edu.au')
-    env.HOUDINI_PATH.set('/opt/carbon/9.2.5/hfs17.5.327/houdini17.5/:&')
+    env.HOUDINI_PATH.append('/opt/carbon/9.2.5/hfs17.5.327/houdini17.5/:&')
     env.PATH.prepend('/opt/carbon/9.2.5/hfs17.5.327/houdini17.5/')
 
     env.CARBON_ROOT.set('/opt/carbon/9.2.5/hfs17.5.327/houdini17.5/')
