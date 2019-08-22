22ffc8a79416870b305db32f120840c7333817ee
Ben Skinner <benjamin.skinner@uts.edu.au>
2019-08-22 19:53:34 +1000

    Disabled katana-3.1.3 variant with usd-19.07

--- usd_katana/19.07/package.py
+++ usd_katana/19.07/package.py
@@ -18,7 +18,7 @@ variants = [
             ['platform-linux', 'arch-x86_64', 'katana-3.0.7'],
             ['platform-linux', 'arch-x86_64', 'katana-3.1.1'],
             ['platform-linux', 'arch-x86_64', 'katana-3.1.2'],
-            ['platform-linux', 'arch-x86_64', 'katana-3.1.3']
+            #['platform-linux', 'arch-x86_64', 'katana-3.1.3'] # Causes build errors with 19.07
            ]
 
 
