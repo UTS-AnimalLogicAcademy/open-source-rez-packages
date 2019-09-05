96367873f56af8c8132a70963611a5d679ce3f86
Daniel Flood <daniel.flood-1@uts.edu.au>
2019-09-05 11:37:14 +1000

    bugfix in pyside package.py - calling env.XDG_DATA_DIRS will throw an exception if we resolve via python api

--- pyside/1.2.4/package.py
+++ pyside/1.2.4/package.py
@@ -9,6 +9,7 @@ variants = [['platform-linux', 'arch-x86_64']]
 def commands():
     env.PATH.append('{this.root}/bin')
     env.LD_LIBRARY_PATH.append('{this.root}/lib')
+    env.XDG_DATA_DIRS.set('$XDG_DATA_DIRS:{this.root}/share')
     env.XDG_DATA_DIRS.append('{this.root}/share')
     env.PYTHONPATH.append('{this.root}/lib64/python2.7/site-packages')
 
