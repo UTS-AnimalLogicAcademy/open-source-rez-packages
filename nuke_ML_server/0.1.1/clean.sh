c8c1dd05177e835526b1d128f6596384f5aa037f
Daniel Flood <daniel.flood-1@uts.edu.au>
2020-01-31 12:36:31 +1100

    added nuke_ML_server

---
+++ nuke_ML_server/0.1.1/clean.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+source version.sh
+
+rm v$VERSION.zip
+rm -rf nuke-ML-server-$VERSION
+rm -rf build
+
---
+++ nuke_ML_server/0.1.1/setup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+source version.sh
+
+wget https://github.com/UTS-AnimalLogicAcademy/nuke-ML-server/archive/v$VERSION.zip
+
+unzip v$VERSION.zip
+
