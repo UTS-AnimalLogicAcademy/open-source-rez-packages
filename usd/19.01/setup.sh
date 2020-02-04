8e3a199c7da810d98d2142e54a2c08a3057c9048
Daniel Flood <daniel.flood-1@uts.edu.au>
2020-02-04 12:45:49 +1100

    added patch files to manually control python shebang path in usd builds

--- usd/19.01/setup.sh
+++ usd/19.01/setup.sh
@@ -5,3 +5,6 @@ source version.sh
 wget https://github.com/PixarAnimationStudios/USD/archive/v$VERSION.zip
 unzip v$VERSION.zip
 
+rm USD-$VERSION/CMakeLists.txt
+cp patch/CMakeLists.txt USD-$VERSION/CMakeLists.txt
+
--- usd/19.11/package.py
+++ usd/19.11/package.py
@@ -2,7 +2,7 @@
 
 name = 'usd'
 
-version = '18.09'
+version = '19.11'
 
 requires = [
     'alembic-1.5',
@@ -10,15 +10,18 @@ requires = [
     'tbb-4.4.6',
     'opensubdiv-3.2',
     'ilmbase-2.2',
-    'jemalloc-4',
     'jinja-2',
+    'jemalloc-4',
     'openexr-2.2',
     'pyilmbase-2.2',
     'materialx',
     'oiio-1.8',
     'ptex-2.0',
     'PyOpenGL',
-    'embree_lib'
+    'embree_lib',
+    'glew',
+    'renderman-22.6',
+    'ocio-1.0.9'
 ]
 
 build_requires = [
@@ -35,4 +38,5 @@ def commands():
     env.PYTHONPATH.append('{root}/lib/python')
     env.LD_LIBRARY_PATH.append('{root}/lib/')
     appendenv('PATH', '{root}/bin/')
-    prependenv('PATH', '/usr/bin/')
+
+
