931968ce7fb8018f82763eba232c75b5cb7fc7e9
Naomi Que <149199@ocelot.ala.int.uts.edu.au>
2020-07-29 09:37:09 +1000

    Added 19.11.3 to KatanaUsdPlugins

---
+++ KatanaUsdPlugins/19.11.3/package.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+name = 'KatanaUsdPlugins'
+
+version = '19.11.3'
+
+#requires = [
+#    'usd-19.11',
+#    'PyOpenGL-3.1.0',
+#    'glew'
+#]
+
+private_build_requires = [
+    'cmake-3.12',
+]
+
+variants = [
+     ['platform-linux', 'arch-x86_64', 'katana-3.6.1']
+]
+
+def commands():
+    env.USE_KATANA_USD.set("1")
+
+
---
+++ KatanaUsdPlugins/19.11.3/version.sh
@@ -0,0 +1,2 @@
+export BUILD_VERSION=19.11_fn3
+export RELEASE_VERSION=19.11.3
