ff3994c37def3c2701e69c092161efc6d6bee0cd
Jonah Newton <jonah.newton@uts.edu.au>
2022-05-10 13:05:26 +1000

    Added USD version 21.05_fn3_py3

--- KatanaUsdPlugins/21.05.3.3/rez-release.sh
+++ KatanaUsdPlugins/21.05.3.3/rez-release.sh
@@ -2,6 +2,9 @@
 
 source version.sh
 
+export CC=/opt/rh/devtoolset-6/root/bin/gcc
+export CXX=/opt/rh/devtoolset-6/root/bin/g++
+
 # this will build (if not built already) and release to the configured rez release location
 # the extra flags are because we are not developing this code with git, we're just building
 # someone else's code.  
