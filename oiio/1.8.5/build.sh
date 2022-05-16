7d508a0215f884416a7c3653cdfbdc4c81575849
Jonah Newton <jonah.newton@uts.edu.au>
2022-05-16 10:48:41 +1000

    Added variant for boost 1.70 and added patch to make build work

--- oiio/1.8.5/build.sh
+++ oiio/1.8.5/build.sh
@@ -2,17 +2,17 @@
 
 tar -xvf Release-1.8.5.tar.gz
 rm oiio-Release-1.8.5/CMakeLists.txt
-cp patch/CMakeLists.txt oiio-Release-1.8.5/
+cp patch/CMakeLists_root.txt oiio-Release-1.8.5/CMakeLists.txt
 rm oiio-Release-1.8.5/src/dicom.imageio/dicominput.cpp
 cp patch/dicominput.cpp oiio-Release-1.8.5/src/dicom.imageio/
-export PACKAGE_INSTALLATION_ROOT=/mnt/ala/software/ext_packages
+export PACKAGE_INSTALLATION_ROOT=~/packages
 export PACKAGE_NAME=oiio
-export PACKAGE_VERSION=1.8.5.1
+export PACKAGE_VERSION=1.8.5.2
 export MAKE_THREADS=28
 
 # build variant 1
 export PACKAGE_VARIANT=platform-linux/arch-x86_64/boost-1.55
-rez-env qt-5 boost-1.55 cmake openexr ilmbase ocio ffmpeg -c ./_rezbuild_.sh
+rez-env qt-5 boost-1.55 cmake openexr ilmbase ocio ffmpeg-3 -c ./_rezbuild_.sh
 
 # clean up
 rm -rf oiio-Release-1.8.5
@@ -20,9 +20,21 @@ rm -rf oiio-Release-1.8.5
 # build variant 2
 tar -xvf Release-1.8.5.tar.gz
 rm oiio-Release-1.8.5/CMakeLists.txt
-cp patch/CMakeLists.txt oiio-Release-1.8.5/
+cp patch/CMakeLists_root.txt oiio-Release-1.8.5/CMakeLists.txt
 rm oiio-Release-1.8.5/src/dicom.imageio/dicominput.cpp
 cp patch/dicominput.cpp oiio-Release-1.8.5/src/dicom.imageio/
 
 export PACKAGE_VARIANT=platform-linux/arch-x86_64/boost-1.61
-rez-env qt-5 boost-1.61 cmake openexr ilmbase ocio ffmpeg -c ./_rezbuild_.sh
+rez-env qt-5 boost-1.61 cmake openexr ilmbase ocio ffmpeg-3 -c ./_rezbuild_.sh
+
+# build variant 3
+tar -xvf Release-1.8.5.tar.gz
+rm oiio-Release-1.8.5/CMakeLists.txt
+cp patch/CMakeLists_root.txt oiio-Release-1.8.5/CMakeLists.txt
+rm oiio-Release-1.8.5/src/python/CMakeLists.txt
+cp patch/CMakeLists_python_boost_1_70.txt oiio-Release-1.8.5/src/python/CMakeLists.txt
+rm oiio-Release-1.8.5/src/dicom.imageio/dicominput.cpp
+cp patch/dicominput.cpp oiio-Release-1.8.5/src/dicom.imageio/
+
+export PACKAGE_VARIANT=platform-linux/arch-x86_64/boost-1.70
+rez-env qt-5 boost-1.70 cmake openexr ilmbase ocio ffmpeg-3 -c ./_rezbuild_.sh
--- oiio/1.8.5/package.py
+++ oiio/1.8.5/package.py
@@ -2,18 +2,19 @@
 
 name = 'oiio'
 
-version = '1.8.5.1'
+version = '1.8.5.2'
 
 requires = [
             'openexr',
             'ilmbase',
             'ocio',
-            'ffmpeg'
+            'ffmpeg-3'
             ]
 
 variants = [
             ['platform-linux', 'arch-x86_64', 'boost-1.55'],
-            ['platform-linux', 'arch-x86_64', 'boost-1.61']
+            ['platform-linux', 'arch-x86_64', 'boost-1.61'],
+	    ['platform-linux', 'arch-x86_64', 'boost-1.70']
            ]
 
 def commands():
