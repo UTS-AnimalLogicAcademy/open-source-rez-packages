866054ec2002d4f7378daa9a08ecf5d875e30f16
Jonah Newton <jonah.newton@uts.edu.au>
2022-05-16 10:51:38 +1000

    Disables katana default usd when loaded

--- KatanaUsdPlugins/21.05.3.3/package.py
+++ KatanaUsdPlugins/21.05.3.3/package.py
@@ -2,7 +2,7 @@
 
 name = 'KatanaUsdPlugins'
 
-version = '21.05.3.3.1'
+version = '21.05.3.3'
 
 private_build_requires = [
     'cmake-3.12',
@@ -14,7 +14,7 @@ variants = [
 ]
 
 def commands():
-    env.USE_KATANA_USD.set("1")
+    env.KATANA_USD_PLUGINS_DISABLED.set("1")
     env.KATANA_RESOURCES.append('{this.root}/plugin')
     env.LD_LIBRARY_PATH.append('{this.root}/lib')
 
