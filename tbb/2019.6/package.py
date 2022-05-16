52be9b0aea65b20c593b13db77334a2eca785f15
Jonah Newton <jonah.newton@uts.edu.au>
2022-05-16 10:54:27 +1000

    Updated gcc library location

--- tbb/2019.6/package.py
+++ tbb/2019.6/package.py
@@ -5,7 +5,9 @@ name = 'tbb'
 version = '2019.6'
 
 def commands():
-    appendenv('LD_LIBRARY_PATH', '{root}/lib/intel64/gcc4.8')
-    env.TBB_LIBRARIES.set('{root}/lib/intel64/gcc4.8')
+    appendenv('LD_LIBRARY_PATH', '{root}/lib/intel64/gcc4.7')
+    env.TBBROOT.set('{root}')
+    env.TBB_LIBRARIES.set('{root}/lib/intel64/gcc4.7')
+    env.TBB_INCLUDE_DIR.set('{root}/include')
 
 
